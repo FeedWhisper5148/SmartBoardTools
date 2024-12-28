fadb03d94e615b178008ef693d009e62dbead868
Qian Yulei <qianyulei@sczx.com>
2024-12-28 09:47:53 +0800

    优化了窗口位置；创建了主窗口

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, screen} = require('electron')
 // const path = require('path')
 // const {Menu, Tray} = require('electron')
 
 app.on('ready', () => {
     //当app准备好后，执行createWindow创建窗口
     const win = new BrowserWindow({
-        width: 1280,//窗口宽度
+        width: 1239,//窗口宽度
         height: 100,//窗口高度
         autoHideMenuBar: true,//自动隐藏菜单档
         alwaysOnTop: true,//置顶
-        x: 0,//窗口位置x坐标
+        x: 100,//窗口位置x坐标
         y: 0,//窗口位置y坐标
         frame: false,//无边框窗口
         transparent: true,//透明
         skipTaskbar: true,//不显示在任务栏
     })
+
+      // 获取屏幕的尺寸
+    const screenSize = screen.getPrimaryDisplay().workAreaSize;
+    
+    // 计算窗口居中的位置
+    const winPos = {
+        x: (screenSize.width - 1239) / 2,
+        y: 0
+    };
+    
+    // 设置窗口的位置
+    win.setBounds(winPos);
+
     //加载一个页面
-    win.loadFile('./pages/index.html')
+    win.loadFile('./pages/ClassSchedule.html')
+    const index = new BrowserWindow({
+        width: 600,//窗口宽度
+        height: 400,//窗口高度
+        autoHideMenuBar: true,//自动隐藏菜单档
+    })
+
+    index.center()
 })
 
 // const trayIconPath = path.join(__dirname, 'icon.png');
--- pages/js/ClassSchedule.js
+++ pages/js/ClassSchedule.js
@@ -74,7 +74,7 @@ const classSchedule = [
         { start: "15:40", end: "16:20", course: "物" },
         { start: "16:30", end: "17:10", course: "物" },
         { start: "18:40", end: "19:50", course: "语" },
-        { start: "20:00", end: "21:10", course: "物" },
+        { start: "20:00", end: "20:25", course: "物" },
         { start: "21:20", end: "10:30", course: "数" }
     ]
 ]
@@ -101,12 +101,13 @@ function getScheduleForToday() {
 function getNextClassInfo() {
     const schedule = getScheduleForToday(); // 获取今天的课表
     // console.log(schedule)
-    // const now = new Date();
-    // const currentHour = now.getHours();
-    // const currentMinute = now.getMinutes();
-    // let currentTimeInSeconds = currentHour * 3600 + currentMinute * 60;
-    // let nextClassIndex = -1;
-    // let nextClassTimeInSeconds = Infinity;
+    const now = new Date();
+    const currentHour = now.getHours();
+    const currentMinute = now.getMinutes();
+    const currentSecond = now.getSeconds();
+    let currentTimeInSeconds = currentHour * 3600 + currentMinute * 60 + currentSecond;
+    let nextClassIndex = -1;
+    let nextClassTimeInSeconds = Infinity;
     for (let i = 0; i < schedule.length; i++) {
         // console.log(schedule[i])
         document.getElementById(`class${i}`).innerHTML = String(schedule[i].course);
@@ -120,11 +121,26 @@ function getNextClassInfo() {
         const startTimeInSeconds = startHour * 3600 + startMinute * 60;
         const endTimeInSeconds = endHour * 3600 + endMinute * 60;
 
+        // 判断当前是否为上课时间，如果是，就隐藏窗口
         if (currentTimeInSeconds >= startTimeInSeconds && currentTimeInSeconds < endTimeInSeconds) {
+            document.getElementById('container').style.visibility = 'hidden'
             return { isClassTime: true, courseIndex: i };
+        // 如果不是，就给下一节课添加动画
         } else if (currentTimeInSeconds < startTimeInSeconds && startTimeInSeconds < nextClassTimeInSeconds) {
             nextClassIndex = i;
+            console.log(`class${i}`)
+            document.getElementById(`class${i}`).setAttribute('class', 'nextCourse')
+            document.getElementById('container').style.visibility = 'visible'
+            document.getElementById(`class${i - 1}`).removeAttribute('class', 'nextCourse')
             nextClassTimeInSeconds = startTimeInSeconds;
+            // 计算到下一节的时间
+            let countDownToNextCourse =startTimeInSeconds - currentTimeInSeconds
+            let countDownToNextCourseInMinutes = Math.floor(countDownToNextCourse / 60)
+            let countDownToNextCourseInSeconds = countDownToNextCourse % 60
+            // console.log(countDownToNextCourseInMinutes)
+            // console.log(countDownToNextCourseInSeconds)
+            // console.log(countDownToNextCourse)
+            document.getElementById('countDownToClass').innerHTML = String(countDownToNextCourseInMinutes) + ':' + String(countDownToNextCourseInSeconds)
         }
     }
 
